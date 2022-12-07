9d48bd013ce5357898e52ddb5e129ff6b0a836c3
yh.wang <yh.wang@high-flyer.cn>
2022-12-07 15:55:42 +0800

    update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 echo 'deb http://dk.archive.ubuntu.com/ubuntu/ xenial main' >> /etc/apt/sources.list && \
 echo 'deb http://dk.archive.ubuntu.com/ubuntu/ xenial universe' >> /etc/apt/sources.list && \
 apt update
-apt install -y g++-5 gcc-5
+apt install -y g++-5 gcc-5 python3-pip
 pip3 install matplotlib
 cd ./analysis && make && cd ../
 cd ./simulation/ && CC='gcc-5' CXX='g++-5' ./waf configure && cd ../
